5b2f9aa4086bbc50d4e7b1782fb646d37125e697
challardo <83609061+challardo@users.noreply.github.com>
2021-05-03 12:00:18 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        
+    
+        <title>Carlitos Cool</title>
+       
+
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  
+       
+        <script type="text/javascript" src="js/smooth-scroll.polyfills.min.js"></script>
+        <script type="text/javascript" src="js/script.js"></script>
+    
+        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&family=Roboto+Condensed:wght@300;700&display=swap" rel="stylesheet"> 
+        <!-- Bootstrap core CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <!-- Custom styles for this template -->
+        <link href="css/style.css" rel="stylesheet">
+         <link href="css/hamburgers.css" rel="stylesheet">
+         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+      </head>
+<header style="background-color:black">
+<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler second-button" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+  <div class="animated-icon2"><span></span><span></span><span></span><span></span></div>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+   
+  </div>
+</nav>
+
+</header>
+
+<body >
+
+
+ 
+
+
+<div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light" id="Jumbotron">
+      <div class="rellax col-md-5 p-lg-5 mx-auto my-5" data-rellax-speed="-6" data-rellax-percentage="0.5" data-rellax-zindex="5">
+        <h1 class="display-3 font-weight-normal">Carlitos Cool</h1>
+        <p class="lead font-weight-normal">Desarollador de paginas ridiculas como esta y mas</p>
+        <a class="btn btn-outline-danger" id="btnJumbotron" href="#">Coming soon</a>
+      </div>
+
+    </div>
+    <div class="panel" data-color="white">
+        <div>
+          <h1>Magic scrolling colours</h1>
+         
+      </div>
+    </div>
+    <div class="panel" data-color="violet">
+      <h2>Violet panel</h2>
+    </div>
+    <div class="panel" data-color="indigo">
+      <h2>Indigo panel</h2>
+    </div>
+    <div class="panel" data-color="blue">
+      <h2>Blue panel</h2>
+    </div>
+    <div class="panel" data-color="green">
+      <h2>Green panel</h2>
+    </div>
+    <div class="panel" data-color="yellow">
+      <h2>Yellow panel</h2>
+    </div>
+    <div class="panel" data-color="orange">
+      <h2>Orange panel</h2>
+    </div>
+    <div class="panel" data-color="red">
+      <h2>Red panel</h2>
+    </div>
+    <div class="panel" data-color="white">
+      <h2>white panel</h2>
+    </div>
+
+    <!--Section: Contact v.2-->
+    
+<section class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+
+<!--Section heading-->
+<h2 class="h1-responsive font-weight-bold text-center my-4">Contact us</h2>
+<!--Section description-->
+<p class="text-center w-responsive mx-auto mb-5">Tienes alguna duda, comentario, aportacion? Si quieres ponerte en contacto no dudes en enviarme un correo</p>
+
+<div class="row">
+
+    <!--Grid column-->
+    <div class="col-md-9 mb-md-0 mb-5">
+        <form id="contact-form" name="contact-form" action="mail.php" method="POST">
+
+            <!--Grid row-->
+            <div class="row">
+
+                <!--Grid column-->
+                <div class="col-md-6">
+                    <div class="md-form mb-0">
+                        <input type="text" id="name" name="name" class="form-control">
+                        <label for="name" class="">Your name</label>
+                    </div>
+                </div>
+                <!--Grid column-->
+
+                <!--Grid column-->
+                <div class="col-md-6">
+                    <div class="md-form mb-0">
+                        <input type="text" id="email" name="email" class="form-control">
+                        <label for="email" class="">Your email</label>
+                    </div>
+                </div>
+                <!--Grid column-->
+
+            </div>
+            <!--Grid row-->
+
+            <!--Grid row-->
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="md-form mb-0">
+                        <input type="text" id="subject" name="subject" class="form-control">
+                        <label for="subject" class="">Subject</label>
+                    </div>
+                </div>
+            </div>
+            <!--Grid row-->
+
+            <!--Grid row-->
+            <div class="row">
+
+                <!--Grid column-->
+                <div class="col-md-12">
+
+                    <div class="md-form">
+                        <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>
+                        <label for="message">Your message</label>
+                    </div>
+
+                </div>
+            </div>
+            <!--Grid row-->
+
+        </form>
+
+        <div class="text-right ">
+            <a class="btn btn-outline-danger " type="button" onclick="document.getElementById('contact-form').submit();">Send</a>
+        </div>
+        <div class="status"></div>
+    </div>
+    <!--Grid column-->
+
+    <!--Grid column-->
+    <div class="col-md-3 text-center">
+        <ul class="list-unstyled mb-0">
+            <li><i class="fas fa-map-marker-alt fa-2x"></i>
+                <p>Baja California, TJ 94126, MX</p>
+            </li>
+
+            <li><i class="fas fa-phone mt-4 fa-2x"></i>
+                <p>+ 01 234 567 89</p>
+            </li>
+
+            <li><i class="fas fa-envelope mt-4 fa-2x"></i>
+                <p>carlitoscool@gmail.com</p>
+            </li>
+        </ul>
+    </div>
+    <!--Grid column-->
+
+</div>
+
+</section>
+<!--Section: Contact v.2-->
+
+
+
+<a class="btn btn-outline-danger px-4 rounded-0 scroll-top" href="#">Back to top</a>
+
+</body>
+<!-- Footer -->
+<footer class="page-footer font-small blue">
+
+  <!-- Copyright -->
+  <div id="footerText" class="footer-copyright text-center py-3">© 2020 Copyright:
+    <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+  
+</html>
+
